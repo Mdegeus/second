a7ff79de90f5488125752b123619fe940c0ebf89
mdegeus <302639117@student.rocmondriaan.nl>
2021-03-15 13:11:22 +0100

    all

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.querySelector('.btn-menu').addEventListener('click', function(){
+    openNav();
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  if (localStorage.getItem('theme') === null){
+    localStorage.setItem('theme', 'theme-dark');
+    }
+
+document.documentElement.className = localStorage.getItem('theme');
---
+++ js/theme.js
@@ -0,0 +1,17 @@
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+document.querySelector('.btn-dark-th').addEventListener('click', function(){
+    setTheme('theme-dark')
+});
+
+document.querySelector('.btn-light-th').addEventListener('click', function(){
+    setTheme('theme-light')
+});
+
+document.querySelector('.btn-blue-th').addEventListener('click', function(){
+    setTheme('theme-blue')
+});

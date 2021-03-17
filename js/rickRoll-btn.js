f5b91ad9129a94e7234a2adc6a2b8bfe63336ee2
mdegeus <302639117@student.rocmondriaan.nl>
2021-03-17 14:37:06 +0100

    audio added!, new js file, audio button

---
+++ js/rickRoll-btn.js
@@ -0,0 +1,16 @@
+const audio = new Audio('audio/RickRoll.mp3');
+
+document.querySelector('.btn-rick-roll').addEventListener('mouseover', function(){
+    audio.play();
+})
+
+document.querySelector('.btn-rick-roll').addEventListener('mouseout', function(){
+    audio.pause();
+})
+
+document.querySelector('.btn-rick-roll').addEventListener('click', function(){
+    window.open(
+        "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        '_blank'
+      );
+})
